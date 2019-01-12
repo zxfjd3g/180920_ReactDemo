2b07491bccf52ff9f106e1e861ecbac4e15840ca
zxfjd3g <258147149@qq.com>
2019-01-12 10:54:21 +0800

    基本路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {NavLink, Route, Switch, Redirect} from 'react-router-dom'
 
-import Search from './components/search'
-import Main from './components/main'
+import About from './views/about'
+import Home from './views/home'
+
 
 /*
 应用根组件
@@ -10,9 +12,35 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <Search/>
-        <Main/>
+      <div>
+        <div class="row">
+          <div class="col-xs-offset-2 col-xs-8">
+            <div class="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-2 col-xs-offset-2">
+            <div class="list-group">
+              {/*定义路由链接*/}
+              <NavLink className='list-group-item' to='/about' activeClassName='activeClass'>About</NavLink>
+              <NavLink className='list-group-item' to='/home' activeClassName='activeClass'>Home</NavLink>
+            </div>
+          </div>
+
+          <div class="col-xs-6">
+            <div class="panel">
+              <div class="panel-body">
+                {/*在此定义所有在此显示的路由*/}
+                <Switch>
+                  <Route path='/about' component={About}/>
+                  <Route path='/home' component={Home}/>
+                  <Redirect to='/about'/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
